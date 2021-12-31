dc7bc83cfa99631b2e1dd32922a368c3d2659e45
Tenacious Dandelion MSc <96745402+TenaciousDandelion@users.noreply.github.com>
2021-12-31 11:26:06 +0100

    adding display list of authors

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -22,6 +22,18 @@ public class Author {
         this.lastName = lastName;
     }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
     @Override
     public String toString() {
         return "Author{" +
